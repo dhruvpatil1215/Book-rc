c81504b57f929e54ed4c2756a3a77ebe854942b1
dhruvpatil1215 <148572263+dhruvpatil1215@users.noreply.github.com>
2024-09-15 11:02:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import random
+
+import os
+
+
+
+
+
+
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           book_name=list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           )
+
+
+
+
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books', methods=['post'])
+def recommend():
+    user_input = request.form.get('user-input')
+    if user_input not in pt.index:
+        random_books = random.sample(list(pt.index), 4)
+        data = []
+        for book in random_books:
+            item = []
+            temp_df = books[books['Book-Title'] == book]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+            data.append(item)
+    else:
+        index = np.where(pt.index == user_input)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+            data.append(item)
+
+    print(data)
+
+    return render_template('recommend.html', data=data)
+
+@app.route('/aboutus')
+def contact_ui():
+    return render_template('aboutus.html')
+
+@app.route('/login')
+def login_ui():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
